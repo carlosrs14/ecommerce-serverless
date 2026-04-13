48fe529fedf3fd1001a01eebe164ea2560107fe1
carlosrs14 <crincones578@gmail.com>
2026-04-13 02:23:07 -0500

    config shared done

--- backend/src/csharp/Ecommerce.Shared/Models/Products/CreateProductRequest.cs
+++ backend/src/csharp/Ecommerce.Shared/Models/Products/CreateProductRequest.cs
@@ -2,5 +2,8 @@ namespace Ecommerce.Shared.Models.Products;
 
 public class CreateProductRequest
 {
-    
+    public string Name { get; set; } = string.Empty;
+    public string Brand { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Category { get; set; } = string.Empty;
 }
--- backend/src/csharp/Ecommerce.Shared/Models/Products/Product.cs
+++ backend/src/csharp/Ecommerce.Shared/Models/Products/Product.cs
@@ -2,14 +2,18 @@ using Amazon.DynamoDBv2.DataModel;
 
 namespace Ecommerce.Shared.Models.Products;
 
-[DynamoDBTable("Ecommerce")]
+[DynamoDBTable("ecommerce")]
 public sealed class Product
 {
     [DynamoDBHashKey]
-    public string PK { get; init; } = string.Empty;
+    public string PK { get; set; } = string.Empty; // PRODUCT#<id>
 
     [DynamoDBRangeKey]
-    public string SK { get; init; } = string.Empty;
+    public string SK { get; set; } = string.Empty; // PRODUCT#<id>
 
-    
+    public string Name { get; set; } = string.Empty;
+    public string Brand { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string? EAN { get; set; }
+    public string Category { get; set; } = string.Empty;
 }
